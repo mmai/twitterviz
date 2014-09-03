26a822c6c2161b6a4bb42b7781cac1de37a9aac8
Henri Bourcereau <henri.bourcereau@gmail.com>
2014-09-03 10:52:33 +0200

    --from option

--- bin/twitterviz.js
+++ bin/twitterviz.js
@@ -13,7 +13,8 @@ var settings = require(__dirname + '/../settings');
 var geo = new Geo(__dirname + '/../node_modules/carmen/tiles');
 
 cli.parse({
-    username:  ['u', 'The twitter user name', 'string'],
+    username:  ['u', 'Twitter user name', 'string'],
+    from:  ['f', 'Start point location', 'string'],
     limit:  ['l', 'Maximum number of followers to fetch', 'int']
   },
   ['farthest', 'worldtour', 'goto']
@@ -21,27 +22,23 @@ cli.parse({
 
 //Check the destination
 if (cli.command === "goto"){
-  var destination_name = cli.args[0];
-  if (!destination_name){
-    cli.error("You must indicate a destination after 'goto'");
-    cli.exit();
-  } else {
-    var found_destination = geo.addGeoloc({location: destination_name})
-    .then(function(data, error){
-        if (error || !data.latitude){
-          cli.error("Could not localize '"+ destination_name + "'");
-          cli.exit();
-        } 
-        return data;
-      });
-  }
+  var found_destination = checkLocation(cli.args[0]).fail(showErrorAndExit);
+}
+
+//Check the origin
+var origin = false;
+if (cli.options.from){
+  checkLocation(cli.options.from).then(function(localized_origin){
+      origin = localized_origin;
+    }).fail(showErrorAndExit);
 }
 
 //Everything ok. We can fetch twitter followers infos. 
 var tw = new Twitter(settings.twitter_credentials);
 
 tw.verifyCredentials().then(function(){
-    var user_promise = tw.getInfos(cli.options.username).then(geo.addGeoloc)
+    var user_promise = tw.getInfos(cli.options.username).fail(showErrorAndExit)
+    .then(geo.addGeoloc);
     // .done();
 
     cli.output("Fetching @" + cli.options.username + " followers...\n");
@@ -87,37 +84,41 @@ tw.verifyCredentials().then(function(){
     // .done();
 
     Q.all([user_promise, followers_promise]).then(function(datas){
-        var user = datas[0];
         var places = datas[1];
+        origin = origin || datas[0];
+        if (!origin.latitude){
+          cli.error("Could not localize @" + cli.options.username + " and no origin location set. Try with the --from option.");
+          cli.exit();
+        }
 
         if (cli.command === 'goto'){
           found_destination.then(function(destination){
               cli.spinner("Calculating itinerary...")
-              var path = geo.gotopath(user, destination, places);
+              var path = geo.gotopath(origin, destination, places);
               cli.spinner("Calculating itinerary... done!\n", true);
               cli.output("\n--------------------------------------------------\n");
-              cli.output("Going from " + user.location + " to " + destination_name + "\n");
+              cli.output("Going from " + origin.location + " to " + destination_name + "\n");
               cli.output("--------------------------------------------------\n\n");
               path.forEach(function(place){
                   cli.output(formatPlaceUsers(place) + "\n");
                 });
               // cli.output(destination_name + "\n");
-            }).done();
+            });
         } else if (cli.command === 'farthest'){
           cli.spinner("Calculating itinerary...")
-          var farthest_place = geo.farthest(user, places);
-          var path = geo.geopath(user, farthest_place, places);
+          var farthest_place = geo.farthest(origin, places);
+          var path = geo.geopath(origin, farthest_place, places);
           path.push(farthest_place);
           cli.spinner("Calculating itinerary... done!\n", true);
           cli.output("\n--------------------------------------------------\n");
-          cli.output("Going from " + user.location + " to " + formatPlaceUsers(farthest_place) + "\n");
+          cli.output("Going from " + origin.location + " to " + formatPlaceUsers(farthest_place) + "\n");
           cli.output("--------------------------------------------------\n\n");
           path.forEach(function(place){
               cli.output(formatPlaceUsers(place) + "\n");
             });
         } else if (cli.command === 'worldtour'){
           cli.spinner("Calculating itinerary...")
-          var worldtour = geo.worldtour(user, places);
+          var worldtour = geo.worldtour(origin, places);
           cli.spinner("Calculating itinerary... done!\n", true);
           cli.output("--------------------------------------------------\n");
           cli.output("Worldtour\n");
@@ -129,6 +130,17 @@ tw.verifyCredentials().then(function(){
       })
     .done();
   }).done();
+
+function checkLocation(placeName){
+  var deferred = Q.defer();
+  geo.addGeoloc({location: placeName}).then(function(data, error){
+      if (error || !data.latitude){
+        deferred.reject("Could not localize '"+ placeName + "'");
+      } 
+      deferred.resolve(data);
+    });
+  return deferred.promise;
+}
 
 function toPlaces(users){
   var placesObj = R.groupBy(function(user){return user.location}, users);
@@ -147,4 +159,9 @@ function formatPlaceUsers(place){
     users = "@" + place.users[0].screen_name + ", @" + place.users[1].screen_name + " and " + (place.users.length - 2) + " others";
   }
   return place.computed_location + " (" + users + ")";
+}
+
+function showErrorAndExit(err){
+  cli.error(err);
+  cli.exit();
 }
